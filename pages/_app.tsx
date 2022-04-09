995ab00d6b357a565a8cf8e7ee0eb66a379360d8
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-04-09 19:16:28 +0100

    skeleton home and contatc page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import Layout from '../components/Layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Layout title='Home Page'>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   );
 }
