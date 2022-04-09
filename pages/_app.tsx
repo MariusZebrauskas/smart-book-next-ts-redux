cce9cb145216465a6a4eff271f6ecb41367f5f9d
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-04-09 16:37:24 +0100

    cleaned up

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+
+// import type { AppProps } from 'next/app'
+
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
+}
