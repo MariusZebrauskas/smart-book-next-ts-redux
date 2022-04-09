7c0ed072a09a67a2693cfaa89664f7b0f6aab365
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-04-09 17:21:42 +0100

    tilewind added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 
 const IndexPage = () => (
   <Layout title='Home Page'>
-    <h1>hello app</h1>
+    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
   </Layout>
 );
 
