2db51e3387f35596ed4afcb50fbe3630ad2f5ade
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-04-11 00:01:43 +0100

    building todo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-white dark:bg-gray-800 mt-20'>
+    <footer className='bg-gray-800 mt-20'>
       <div className='container px-6 py-8 mx-auto'>
         <div className='text-center'>
           <a
             href='#'
-            className='text-2xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300'
+            className='text-2xl font-bold text-white hover:text-gray-300'
           >
             Smart Book
           </a>
 
-          <p className='max-w-md mx-auto mt-2 text-gray-500 dark:text-gray-400'>
+          <p className='max-w-md mx-auto mt-2 text-gray-400'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
 
          
         </div>
 
-        <hr className='my-10 border-gray-200 dark:border-gray-700' />
+        <hr className='my-10 border-gray-700' />
 
         <div className='flex flex-col items-center sm:flex-row sm:justify-between'>
           <p className='text-sm text-gray-400'>© Copyright 2021. All Rights Reserved.</p>
@@ -27,7 +27,7 @@ const Footer = () => {
           <div className='flex mt-3 -mx-2 sm:mt-0'>
             <a
               href='#'
-              className='mx-2 text-sm text-gray-400 hover:text-gray-500 dark:hover:text-gray-300'
+              className='mx-2 text-sm text-gray-400  hover:text-gray-300'
               aria-label='Reddit'
             >
               {' '}
---
+++ components/SearchInput.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const SearchInput = () => {
+  return (
+    <div className='flex justify-center mt-20 w-full '>
+      <div className='mb-3 xl:w-96  w-full'>
+        <div className='relative flex  items-stretch w-full mb-4 rounded '>
+          <input
+            type='search'
+            className='form-control relative flex-auto min-w-0  w-full px-3 py-1.5 text-base font-normal 
+            text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition 
+            ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+            placeholder='Add a todo!'
+            aria-label='Search'
+            aria-describedby='button-addon2'
+          />
+          <button
+            type='button'
+            class='px-6 py-2 border-2 border-gray-800 text-gray-800 font-medium text-xs leading-tight 
+            uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 
+            transition duration-150 ease-in-out'
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;
