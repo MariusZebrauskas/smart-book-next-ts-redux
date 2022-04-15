5df70b6d5f2fd19b66ec07fd5fd4114fefcb71b3
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-04-15 20:35:00 +0100

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@
 
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
+
+// midlewere
+
+app.use(express.json());
 
 // routes
 
@@ -12,9 +17,26 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'hey im here' });
 });
 
+app.post('/', (req, res) => {
+  const { name } = req.body;
+  console.log('name:', name);
 
-app.listen(process.env.PORT, () => {
-    console.log('app is running on port: ', + process.env.PORT);
+  res.status(200).json({ message: 'post me data', name: name });
 });
 
 
+
+
+
+// db connect
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log('db connected');
+      app.listen(process.env.PORT, () => {
+      console.log('app is running on port: ', +process.env.PORT, '+ db connected');
+    });
+    })
+    .catch((err) => {
+      console.log('mongo error', err);
+    });
