9783c5b7c14300395b55bb3de1ab0cb1e577def1
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-04-15 19:15:28 +0100

    moved in to client folder

--- components/LogoText.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LogoText = () => {
-  return (
-    <h4 className='font-extrabold logo  tracking-wider text-2xl font-mono text-white'>SmartBook</h4>
-  );
-};
-
-export default LogoText;
