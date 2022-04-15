b0ef7c93623824e1f11e5de301ab0ce2e1836b51
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-04-15 23:03:59 +0100

    working on register api

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const register = require('./routes/register');
 
 const app = express();
 
@@ -11,32 +12,21 @@ const app = express();
 
 app.use(express.json());
 
+
 // routes
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hey im here' });
-});
-
-app.post('/', (req, res) => {
-  const { name } = req.body;
-  console.log('name:', name);
-
-  res.status(200).json({ message: 'post me data', name: name });
-});
-
-
+app.use("/api", register);
 
 
 
 // db connect
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-      console.log('db connected');
-      app.listen(process.env.PORT, () => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('db connected');
+    app.listen(process.env.PORT || 5000, () => {
       console.log('app is running on port: ', +process.env.PORT, '+ db connected');
     });
-    })
-    .catch((err) => {
-      console.log('mongo error', err);
-    });
+  })
+  .catch((err) => {
+    console.log('mongo error', err);
+  });
